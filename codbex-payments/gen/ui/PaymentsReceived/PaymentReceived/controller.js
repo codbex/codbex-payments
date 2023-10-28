8d045a175aa02ce122638ad052d4636fa162437b
delchev <delchevn@gmail.com>
2023-10-28 12:39:45 +0300

    renaming perspectives phase II

---
+++ codbex-payments/gen/ui/PaymentsReceived/PaymentReceived/controller.js
@@ -0,0 +1,201 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-payments.PaymentsReceived.PaymentReceived';
+	}])
+	.config(["entityApiProvider", function (entityApiProvider) {
+		entityApiProvider.baseUrl = "/services/js/codbex-payments/gen/api/PaymentsReceived/PaymentReceived.js";
+	}])
+	.controller('PageController', ['$scope', '$http', 'messageHub', 'entityApi', function ($scope, $http, messageHub, entityApi) {
+
+		$scope.dataPage = 1;
+		$scope.dataCount = 0;
+		$scope.dataOffset = 0;
+		$scope.dataLimit = 10;
+		$scope.action = "select";
+
+		function refreshData() {
+			$scope.dataReset = true;
+			$scope.dataPage--;
+		}
+
+		//-----------------Events-------------------//
+		messageHub.onDidReceiveMessage("clearDetails", function (msg) {
+			$scope.$apply(function () {
+				$scope.selectedEntity = null;
+				$scope.action = "select";
+			});
+		});
+
+		messageHub.onDidReceiveMessage("entityCreated", function (msg) {
+			refreshData();
+			$scope.loadPage();
+		});
+
+		messageHub.onDidReceiveMessage("entityUpdated", function (msg) {
+			refreshData();
+			$scope.loadPage();
+		});
+		//-----------------Events-------------------//
+
+		$scope.loadPage = function () {
+			$scope.selectedEntity = null;
+			entityApi.count().then(function (response) {
+				if (response.status != 200) {
+					messageHub.showAlertError("PaymentReceived", `Unable to count PaymentReceived: '${response.message}'`);
+					return;
+				}
+				$scope.dataCount = response.data;
+				$scope.dataPages = Math.ceil($scope.dataCount / $scope.dataLimit);
+				let offset = ($scope.dataPage - 1) * $scope.dataLimit;
+				let limit = $scope.dataLimit;
+				if ($scope.dataReset) {
+					offset = 0;
+					limit = $scope.dataPage * $scope.dataLimit;
+				}
+				entityApi.list(offset, limit).then(function (response) {
+					if (response.status != 200) {
+						messageHub.showAlertError("PaymentReceived", `Unable to list PaymentReceived: '${response.message}'`);
+						return;
+					}
+					if ($scope.data == null || $scope.dataReset) {
+						$scope.data = [];
+						$scope.dataReset = false;
+					}
+
+					response.data.forEach(e => {
+						if (e.Date) {
+							e.Date = new Date(e.Date);
+						}
+						if (e.ValueDate) {
+							e.ValueDate = new Date(e.ValueDate);
+						}
+					});
+
+					$scope.data = $scope.data.concat(response.data);
+					$scope.dataPage++;
+				});
+			});
+		};
+		$scope.loadPage($scope.dataPage);
+
+		$scope.selectEntity = function (entity) {
+			$scope.selectedEntity = entity;
+			messageHub.postMessage("entitySelected", {
+				entity: entity,
+				selectedMainEntityId: entity.Id,
+				optionsCustomer: $scope.optionsCustomer,
+				optionsCurrency: $scope.optionsCurrency,
+				optionsOperator: $scope.optionsOperator,
+			});
+		};
+
+		$scope.createEntity = function () {
+			$scope.selectedEntity = null;
+			$scope.action = "create";
+
+			messageHub.postMessage("createEntity", {
+				entity: {},
+				optionsCustomer: $scope.optionsCustomer,
+				optionsCurrency: $scope.optionsCurrency,
+				optionsOperator: $scope.optionsOperator,
+			});
+		};
+
+		$scope.updateEntity = function () {
+			$scope.action = "update";
+			messageHub.postMessage("updateEntity", {
+				entity: $scope.selectedEntity,
+				optionsCustomer: $scope.optionsCustomer,
+				optionsCurrency: $scope.optionsCurrency,
+				optionsOperator: $scope.optionsOperator,
+			});
+		};
+
+		$scope.deleteEntity = function () {
+			let id = $scope.selectedEntity.Id;
+			messageHub.showDialogAsync(
+				'Delete PaymentReceived?',
+				`Are you sure you want to delete PaymentReceived? This action cannot be undone.`,
+				[{
+					id: "delete-btn-yes",
+					type: "emphasized",
+					label: "Yes",
+				},
+				{
+					id: "delete-btn-no",
+					type: "normal",
+					label: "No",
+				}],
+			).then(function (msg) {
+				if (msg.data === "delete-btn-yes") {
+					entityApi.delete(id).then(function (response) {
+						if (response.status != 204) {
+							messageHub.showAlertError("PaymentReceived", `Unable to delete PaymentReceived: '${response.message}'`);
+							return;
+						}
+						refreshData();
+						$scope.loadPage($scope.dataPage);
+						messageHub.postMessage("clearDetails");
+					});
+				}
+			});
+		};
+
+		//----------------Dropdowns-----------------//
+		$scope.optionsCustomer = [];
+		$scope.optionsCurrency = [];
+		$scope.optionsOperator = [];
+
+		$http.get("/services/js/codbex-payments/gen/api/partners/Customer.js").then(function (response) {
+			$scope.optionsCustomer = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+
+		$http.get("/services/js/codbex-payments/gen/api/entities/Currency.js").then(function (response) {
+			$scope.optionsCurrency = response.data.map(e => {
+				return {
+					value: e.Code,
+					text: e.Code
+				}
+			});
+		});
+
+		$http.get("/services/js/codbex-payments/gen/api/Employees/Employee.js").then(function (response) {
+			$scope.optionsOperator = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+		$scope.optionsCustomerValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsCustomer.length; i++) {
+				if ($scope.optionsCustomer[i].value === optionKey) {
+					return $scope.optionsCustomer[i].text;
+				}
+			}
+			return null;
+		};
+		$scope.optionsCurrencyValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsCurrency.length; i++) {
+				if ($scope.optionsCurrency[i].value === optionKey) {
+					return $scope.optionsCurrency[i].text;
+				}
+			}
+			return null;
+		};
+		$scope.optionsOperatorValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsOperator.length; i++) {
+				if ($scope.optionsOperator[i].value === optionKey) {
+					return $scope.optionsOperator[i].text;
+				}
+			}
+			return null;
+		};
+		//----------------Dropdowns-----------------//
+
+	}]);
