f61d34ebef63ccae1c00168396fc29d1348226ee
MinaDoncheva <mina.doncheva@icloud.com>
2024-06-27 13:44:34 +0300

    regenerate from edm

---
+++ codbex-payments/gen/codbex-payments/ui/Settings/PaymentRecordDirection/controller.js
@@ -0,0 +1,163 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-payments.Settings.PaymentRecordDirection';
+	}])
+	.config(["entityApiProvider", function (entityApiProvider) {
+		entityApiProvider.baseUrl = "/services/ts/codbex-payments/gen/codbex-payments/api/Settings/PaymentRecordDirectionService.ts";
+	}])
+	.controller('PageController', ['$scope', 'messageHub', 'entityApi', 'Extensions', function ($scope, messageHub, entityApi, Extensions) {
+
+		$scope.dataPage = 1;
+		$scope.dataCount = 0;
+		$scope.dataLimit = 20;
+
+		//-----------------Custom Actions-------------------//
+		Extensions.get('dialogWindow', 'codbex-payments-custom-action').then(function (response) {
+			$scope.pageActions = response.filter(e => e.perspective === "Settings" && e.view === "PaymentRecordDirection" && (e.type === "page" || e.type === undefined));
+			$scope.entityActions = response.filter(e => e.perspective === "Settings" && e.view === "PaymentRecordDirection" && e.type === "entity");
+		});
+
+		$scope.triggerPageAction = function (action) {
+			messageHub.showDialogWindow(
+				action.id,
+				{},
+				null,
+				true,
+				action
+			);
+		};
+
+		$scope.triggerEntityAction = function (action) {
+			messageHub.showDialogWindow(
+				action.id,
+				{
+					id: $scope.entity.Id
+				},
+				null,
+				true,
+				action
+			);
+		};
+		//-----------------Custom Actions-------------------//
+
+		function resetPagination() {
+			$scope.dataPage = 1;
+			$scope.dataCount = 0;
+			$scope.dataLimit = 20;
+		}
+		resetPagination();
+
+		//-----------------Events-------------------//
+		messageHub.onDidReceiveMessage("entityCreated", function (msg) {
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+
+		messageHub.onDidReceiveMessage("entityUpdated", function (msg) {
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+
+		messageHub.onDidReceiveMessage("entitySearch", function (msg) {
+			resetPagination();
+			$scope.filter = msg.data.filter;
+			$scope.filterEntity = msg.data.entity;
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+		//-----------------Events-------------------//
+
+		$scope.loadPage = function (pageNumber, filter) {
+			if (!filter && $scope.filter) {
+				filter = $scope.filter;
+			}
+			$scope.dataPage = pageNumber;
+			entityApi.count(filter).then(function (response) {
+				if (response.status != 200) {
+					messageHub.showAlertError("PaymentRecordDirection", `Unable to count PaymentRecordDirection: '${response.message}'`);
+					return;
+				}
+				if (response.data) {
+					$scope.dataCount = response.data;
+				}
+				let offset = (pageNumber - 1) * $scope.dataLimit;
+				let limit = $scope.dataLimit;
+				let request;
+				if (filter) {
+					filter.$offset = offset;
+					filter.$limit = limit;
+					request = entityApi.search(filter);
+				} else {
+					request = entityApi.list(offset, limit);
+				}
+				request.then(function (response) {
+					if (response.status != 200) {
+						messageHub.showAlertError("PaymentRecordDirection", `Unable to list/filter PaymentRecordDirection: '${response.message}'`);
+						return;
+					}
+					$scope.data = response.data;
+				});
+			});
+		};
+		$scope.loadPage($scope.dataPage, $scope.filter);
+
+		$scope.selectEntity = function (entity) {
+			$scope.selectedEntity = entity;
+		};
+
+		$scope.openDetails = function (entity) {
+			$scope.selectedEntity = entity;
+			messageHub.showDialogWindow("PaymentRecordDirection-details", {
+				action: "select",
+				entity: entity,
+			});
+		};
+
+		$scope.openFilter = function (entity) {
+			messageHub.showDialogWindow("PaymentRecordDirection-filter", {
+				entity: $scope.filterEntity,
+			});
+		};
+
+		$scope.createEntity = function () {
+			$scope.selectedEntity = null;
+			messageHub.showDialogWindow("PaymentRecordDirection-details", {
+				action: "create",
+				entity: {},
+			}, null, false);
+		};
+
+		$scope.updateEntity = function (entity) {
+			messageHub.showDialogWindow("PaymentRecordDirection-details", {
+				action: "update",
+				entity: entity,
+			}, null, false);
+		};
+
+		$scope.deleteEntity = function (entity) {
+			let id = entity.Id;
+			messageHub.showDialogAsync(
+				'Delete PaymentRecordDirection?',
+				`Are you sure you want to delete PaymentRecordDirection? This action cannot be undone.`,
+				[{
+					id: "delete-btn-yes",
+					type: "emphasized",
+					label: "Yes",
+				},
+				{
+					id: "delete-btn-no",
+					type: "normal",
+					label: "No",
+				}],
+			).then(function (msg) {
+				if (msg.data === "delete-btn-yes") {
+					entityApi.delete(id).then(function (response) {
+						if (response.status != 204) {
+							messageHub.showAlertError("PaymentRecordDirection", `Unable to delete PaymentRecordDirection: '${response.message}'`);
+							return;
+						}
+						$scope.loadPage($scope.dataPage, $scope.filter);
+						messageHub.postMessage("clearDetails");
+					});
+				}
+			});
+		};
+
+	}]);
