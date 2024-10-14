f459e2cabf1562f624139d21ec51d27ec20c575f
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-10-14 16:36:34 +0300

    Make employee payment master entity (#33)

---
+++ codbex-payments/gen/codbex-payments/ui/EmployeePayment/EmployeePayment/main-details/controller.js
@@ -0,0 +1,117 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-payments.EmployeePayment.EmployeePayment';
+	}])
+	.config(["entityApiProvider", function (entityApiProvider) {
+		entityApiProvider.baseUrl = "/services/ts/codbex-payments/gen/codbex-payments/api/EmployeePayment/EmployeePaymentService.ts";
+	}])
+	.controller('PageController', ['$scope', 'Extensions', 'messageHub', 'entityApi', function ($scope, Extensions, messageHub, entityApi) {
+
+		$scope.entity = {};
+		$scope.forms = {
+			details: {},
+		};
+		$scope.formHeaders = {
+			select: "EmployeePayment Details",
+			create: "Create EmployeePayment",
+			update: "Update EmployeePayment"
+		};
+		$scope.action = 'select';
+
+		//-----------------Custom Actions-------------------//
+		Extensions.get('dialogWindow', 'codbex-payments-custom-action').then(function (response) {
+			$scope.entityActions = response.filter(e => e.perspective === "EmployeePayment" && e.view === "EmployeePayment" && e.type === "entity");
+		});
+
+		$scope.triggerEntityAction = function (action) {
+			messageHub.showDialogWindow(
+				action.id,
+				{
+					id: $scope.entity.Id
+				},
+				null,
+				true,
+				action
+			);
+		};
+		//-----------------Custom Actions-------------------//
+
+		//-----------------Events-------------------//
+		messageHub.onDidReceiveMessage("clearDetails", function (msg) {
+			$scope.$apply(function () {
+				$scope.entity = {};
+				$scope.optionsCurrency = [];
+				$scope.optionsCompany = [];
+				$scope.action = 'select';
+			});
+		});
+
+		messageHub.onDidReceiveMessage("entitySelected", function (msg) {
+			$scope.$apply(function () {
+				if (msg.data.entity.Date) {
+					msg.data.entity.Date = new Date(msg.data.entity.Date);
+				}
+				if (msg.data.entity.Valor) {
+					msg.data.entity.Valor = new Date(msg.data.entity.Valor);
+				}
+				$scope.entity = msg.data.entity;
+				$scope.optionsCurrency = msg.data.optionsCurrency;
+				$scope.optionsCompany = msg.data.optionsCompany;
+				$scope.action = 'select';
+			});
+		});
+
+		messageHub.onDidReceiveMessage("createEntity", function (msg) {
+			$scope.$apply(function () {
+				$scope.entity = {};
+				$scope.optionsCurrency = msg.data.optionsCurrency;
+				$scope.optionsCompany = msg.data.optionsCompany;
+				$scope.action = 'create';
+			});
+		});
+
+		messageHub.onDidReceiveMessage("updateEntity", function (msg) {
+			$scope.$apply(function () {
+				if (msg.data.entity.Date) {
+					msg.data.entity.Date = new Date(msg.data.entity.Date);
+				}
+				if (msg.data.entity.Valor) {
+					msg.data.entity.Valor = new Date(msg.data.entity.Valor);
+				}
+				$scope.entity = msg.data.entity;
+				$scope.optionsCurrency = msg.data.optionsCurrency;
+				$scope.optionsCompany = msg.data.optionsCompany;
+				$scope.action = 'update';
+			});
+		});
+		//-----------------Events-------------------//
+
+		$scope.create = function () {
+			entityApi.create($scope.entity).then(function (response) {
+				if (response.status != 201) {
+					messageHub.showAlertError("EmployeePayment", `Unable to create EmployeePayment: '${response.message}'`);
+					return;
+				}
+				messageHub.postMessage("entityCreated", response.data);
+				messageHub.postMessage("clearDetails", response.data);
+				messageHub.showAlertSuccess("EmployeePayment", "EmployeePayment successfully created");
+			});
+		};
+
+		$scope.update = function () {
+			entityApi.update($scope.entity.Id, $scope.entity).then(function (response) {
+				if (response.status != 200) {
+					messageHub.showAlertError("EmployeePayment", `Unable to update EmployeePayment: '${response.message}'`);
+					return;
+				}
+				messageHub.postMessage("entityUpdated", response.data);
+				messageHub.postMessage("clearDetails", response.data);
+				messageHub.showAlertSuccess("EmployeePayment", "EmployeePayment successfully updated");
+			});
+		};
+
+		$scope.cancel = function () {
+			messageHub.postMessage("clearDetails");
+		};
+
+	}]);
