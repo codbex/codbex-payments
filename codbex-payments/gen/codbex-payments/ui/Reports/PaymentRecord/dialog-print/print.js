b886c8120a5970f587c519863068486632c98e8b
StanZGenchev <stan.z.genchev@gmail.com>
2025-04-11 14:39:42 +0300

    Fixed some dialogs

--- codbex-payments/gen/codbex-payments/ui/Reports/PaymentRecord/dialog-print/print.js
+++ codbex-payments/gen/codbex-payments/ui/Reports/PaymentRecord/dialog-print/print.js
@@ -1,8 +1,8 @@
 const viewData = {
     id: 'codbex-payments-Reports-PaymentRecord-print',
     label: 'Print',
-    path: '/services/web/codbex-payments/gen/codbex-payments/ui/${perspectiveName}/PaymentRecord/dialog-print/index.html',
-    perspective: '${perspectiveName}',
+    path: '/services/web/codbex-payments/gen/codbex-payments/ui/Reports/PaymentRecord/dialog-print/index.html',
+    perspective: 'Reports',
     view: 'PaymentRecord',
     type: 'page',
     order: 10
